80d14034c74d6931f83e7465a0117c6157067817
Erwin <erwinpardoq@hotmail.com>
2020-11-30 17:58:35 -0500

    movies filter section

--- NgCinemax/src/app/app.module.ts
+++ NgCinemax/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavBarSocialComponent } from './landing-page/nav-bar-social/nav-bar-social.component';
 import { NavbarMenuComponent } from './landing-page/navbar-menu/navbar-menu.component';
+import { MoviesFilterComponent } from './landing-page/movies-filter/movies-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     NavBarSocialComponent,
-    NavbarMenuComponent
+    NavbarMenuComponent,
+    MoviesFilterComponent
   ],
   imports: [
     BrowserModule,
