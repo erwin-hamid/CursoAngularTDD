d1c100e0480e19823447e42e976b5c271b8afa32
Erwin <erwinpardoq@hotmail.com>
2020-11-30 16:42:56 -0500

    NavBar Social

--- NgCinemax/src/app/app.module.ts
+++ NgCinemax/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarSocialComponent } from './landing-page/nav-bar-social/nav-bar-social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavBarSocialComponent
   ],
   imports: [
     BrowserModule,
